b21885b3a8212a16bc01d85b210912ad5156fcb1
ziontee113 <ziontee113@gmail.com>
2022-11-02 22:10:24 +0700

    Add reload keymap in init.lua

--- lua/zion-kit/init.lua
+++ lua/zion-kit/init.lua
@@ -1,7 +1,16 @@
 local M = {}
+local Window = require("zion-kit.lib.window")
 
-M.hello = function()
-	print("hello from zion-kit")
-end
+vim.keymap.set("n", "<A-=>", function()
+	local win = Window:new()
+	win:open()
+end, {})
+
+vim.keymap.set("n", "<A-BS>", function()
+	---@diagnostic disable-next-line: undefined-global
+	R("zion-kit")
+	---@diagnostic disable-next-line: undefined-global
+	N("zion-kit reloaded")
+end, {})
 
 return M
