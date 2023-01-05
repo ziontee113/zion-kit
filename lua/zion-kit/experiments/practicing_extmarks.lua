21db69d1f77b921039318849a0a44c193304c798
ziontee113 <ziontee113@gmail.com>
2023-01-06 01:26:13 +0700

    Remove label when selected

--- lua/zion-kit/experiments/practicing_extmarks.lua
+++ lua/zion-kit/experiments/practicing_extmarks.lua
@@ -28,6 +28,7 @@ REMAP("n", "<Plug>L1 G, R1 P<Plug>", function()
     }
 
     local hash_table = {}
+    local extmark_hash_table = {}
     local count = 1
     for _, matches, _ in iter_query:iter_matches(root, 0) do
         local node = matches[1]
@@ -36,16 +37,18 @@ REMAP("n", "<Plug>L1 G, R1 P<Plug>", function()
         -- print(node_text)
 
         local start_row, start_col, end_row, end_col = node:range()
-        vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
-            virt_text = { { labels[count], "GruvboxAquaBold" } },
-            virt_text_pos = "overlay",
-        })
-        vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
-            virt_text = { { "<-- " .. labels[count], "GruvboxAquaBold" } },
-            priority = 450,
-        })
+        local extmark_id =
+            vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
+                virt_text = { { labels[count], "GruvboxAquaBold" } },
+                virt_text_pos = "overlay",
+            })
+        -- vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
+        --     virt_text = { { "<-- " .. labels[count], "GruvboxAquaBold" } },
+        --     priority = 450,
+        -- })
 
         hash_table[labels[count]] = node
+        extmark_hash_table[labels[count]] = extmark_id
         count = count + 1
 
         --------------------
@@ -58,21 +61,23 @@ REMAP("n", "<Plug>L1 G, R1 P<Plug>", function()
         if ok then
             local key = string.char(keynum)
 
-            if key == "" then break end
+            if key == "" then
+                vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+                break
+            end
 
             local node = hash_table[key]
             if node then
                 local start_row, start_col, end_row, end_col = node:range()
-                vim.highlight.range(
-                    0,
-                    ns,
-                    "Visual",
-                    { start_row, start_col },
-                    { end_row, end_col },
-                    {
-                        priority = 500,
-                    }
-                )
+                vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
+                    end_row = end_row,
+                    end_col = end_col,
+                    hl_group = "Visual",
+                    hl_mode = "replace",
+                    priority = 500,
+                })
+
+                vim.api.nvim_buf_del_extmark(0, ns, extmark_hash_table[key])
             end
 
             vim.cmd "redraw"
