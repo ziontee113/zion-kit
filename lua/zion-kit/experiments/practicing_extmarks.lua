f4b23e88abe6d259b0e85226343b909a215f34be
ziontee113 <ziontee113@gmail.com>
2023-01-06 12:43:15 +0700

    Add combine_hl_groups function

--- lua/zion-kit/experiments/practicing_extmarks.lua
+++ lua/zion-kit/experiments/practicing_extmarks.lua
@@ -2,10 +2,25 @@ require "zion-kit.experiments.ts_node_in_range_predicates"
 
 local ns = vim.api.nvim_create_namespace "ts_testing"
 
-vim.api.nvim_set_hl(0, "AquaVisual", {
-    fg = "#8ec07c",
-    -- fg = "#fe8019",
-    bg = "#665c54",
+local combine_hl_groups =
+    function(fg_group, bg_group, output_group_name, output_group_properties)
+        local foreground_rgb =
+            vim.api.nvim_get_hl_by_name(fg_group, true).foreground
+        local foreground_hex = string.format("#%06x", foreground_rgb)
+
+        local background_rgb =
+            vim.api.nvim_get_hl_by_name(bg_group, true).background
+        local background_hex = string.format("#%06x", background_rgb)
+
+        output_group_properties = output_group_properties
+        output_group_properties.fg = foreground_hex
+        output_group_properties.bg = background_hex
+
+        vim.api.nvim_set_hl(0, output_group_name, output_group_properties)
+        return output_group_name
+    end
+
+combine_hl_groups("GruvboxAqua", "Visual", "AquaVisual", {
     bold = true,
 })
 
@@ -17,8 +32,14 @@ REMAP("n", "<Plug>L1 G, R1 P<Plug>", function()
 
     local query = [[
         ;; query
-        ( (variable_declaration) @cap (#visible-in-view? @cap) )
+        ( "string_content" @cap (#visible-in-view? @cap) )
     ]]
+
+    -- local query = [[
+    --     ;; query
+    --     ( (variable_declaration) @cap (#visible-in-view? @cap) )
+    -- ]]
+
     local iter_query = vim.treesitter.query.parse_query("lua", query)
 
     local parser = vim.treesitter.get_parser(0, "lua")
@@ -32,33 +53,27 @@ REMAP("n", "<Plug>L1 G, R1 P<Plug>", function()
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
         "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ";", "!",
+        -- "/", ".", ":", "'", '"', "\\", "=", "-", "_", "[", "]", "{", "}",
     }
 
     local hash_table = {}
     local extmark_hash_table = {}
     local count = 1
     for _, matches, _ in iter_query:iter_matches(root, 0) do
-        local node = matches[1]
+        if labels[count] then
+            local node = matches[1]
 
-        -- local node_text = vim.treesitter.query.get_node_text(node, 0)
-        -- print(node_text)
+            local start_row, start_col, end_row, end_col = node:range()
+            local extmark_id =
+                vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
+                    virt_text = { { labels[count], "GruvboxOrangeBold" } },
+                    virt_text_pos = "overlay",
+                })
 
-        local start_row, start_col, end_row, end_col = node:range()
-        local extmark_id =
-            vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
-                virt_text = { { labels[count], "GruvboxAquaBold" } },
-                virt_text_pos = "overlay",
-            })
-        -- vim.api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
-        --     virt_text = { { "<-- " .. labels[count], "GruvboxAquaBold" } },
-        --     priority = 450,
-        -- })
-
-        hash_table[labels[count]] = node
-        extmark_hash_table[labels[count]] = extmark_id
-        count = count + 1
-
-        --------------------
+            hash_table[labels[count]] = node
+            extmark_hash_table[labels[count]] = extmark_id
+            count = count + 1
+        end
     end
 
     vim.cmd "redraw"
